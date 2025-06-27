c86600b4be917cd2f678d1a225474f36d70684d1
Jadi <127825377+jawadmunir158@users.noreply.github.com>
2025-06-27 23:38:18 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCollection from '../components/latestCollection'
+import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OuPolicy from '../components/OuPolicy'
 import NewsLetterBox from '../components/NewsLetterBox'
